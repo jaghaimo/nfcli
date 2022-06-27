74624dd6453ea280f3354de656bae2f8ea82c033
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-06-27 15:57:49 +0100

    Style improvements

--- nfcli/printer.py
+++ nfcli/printer.py
@@ -10,16 +10,16 @@ from nfcli.model import Fleet, Ship, Socket
 
 def add_components(tree: Tree, socket: Socket):
     for content in socket.contents:
-        tree.add(f"{content.name} x{content.quantity}")
+        tree.add(f"{content.name} [yellow]x{content.quantity}[/yellow]")
 
 
 def add_sockets(tree: Tree, sockets: List[Socket]):
     for socket in sockets:
-        subtree = tree.add(socket.name)
+        subtree = tree.add(f"{socket.name}")
         add_components(subtree, socket)
 
 def get_ship(ship: Ship):
-    tree = Tree(f"[yellow]{ship.name}[/yellow] [white]({ship.cost} points)[/white]")
+    tree = Tree(f"[red]{ship.name}[/red]\n[green]{ship.hull}[/green]\n[blue]{ship.cost} points[/blue]")
     add_sockets(tree, ship.sockets)
     return tree
 
@@ -27,7 +27,7 @@ def print_fleet(fleet: Fleet):
     console = Console()
     console.print()
     console.print(f"Fleet Name: [cyan]{fleet.name}[/cyan]")
-    console.print(f"Total points: [cyan]{fleet.points}[/cyan]")
+    console.print(f"Total Points: [cyan]{fleet.points}[/cyan]")
     ships = [Padding(get_ship(ship), (1,2)) for ship in fleet.ships]
     console.print(Columns(ships))
 
