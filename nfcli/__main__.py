9c354e1276724631fcc8211a04767a28b6186a47
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-07-02 12:18:50 +0100

    Add data updater stub

--- nfcli/__main__.py
+++ nfcli/__main__.py
@@ -5,6 +5,7 @@ from typing import Dict
 
 from nfcli import load_path
 from nfcli.database import init_database
+from nfcli.importer import update
 from nfcli.parser import parse_any
 from nfcli.printer import printer_factory
 from nfcli.writer import determine_output_file
@@ -15,10 +16,9 @@ DESC = """Command line interface for converting Nebulous: Fleet Command fleet an
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description=DESC)
     parser.add_argument("-d", "--debug", action="store_true", help="enable debug mode")
-    parser.add_argument(
-        "-i", "--input", type=str, required=True, help="fleet or ship file to convert"
-    )
+    parser.add_argument("-i", "--input", type=str, help="fleet or ship file to convert")
     parser.add_argument("-p", "--print", action="store_true", help="print output to console")
+    parser.add_argument("-w", "--write", action="store_true", help="write output to a file")
     parser.add_argument(
         "-s",
         "--style",
@@ -26,7 +26,12 @@ def get_parser() -> argparse.ArgumentParser:
         default="auto",
         help="printer style: auto (default), column, stack",
     )
-    parser.add_argument("-w", "--write", action="store_true", help="write output to a file")
+    parser.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="update internal database from wiki data",
+    )
     return parser
 
 
@@ -41,14 +46,20 @@ def parse_args() -> Dict:
 def main() -> int:
     init_database()
     args = parse_args()
-    xml_data = load_path(args.input)
-    entity = parse_any(args.input, xml_data)
-    if args.print:
-        printer = printer_factory(args.style, entity)
-        entity.print(printer)
-    if args.write:
-        output_file = determine_output_file(args.input, ".png")
-        entity.write(output_file)
+    if args.input:
+        xml_data = load_path(args.input)
+        entity = parse_any(args.input, xml_data)
+        if args.print:
+            printer = printer_factory(args.style, entity)
+            entity.print(printer)
+        if args.write:
+            output_file = determine_output_file(args.input, ".png")
+            entity.write(output_file)
+    elif args.update:
+        update()
+    else:
+        parser = get_parser()
+        parser.print_help()
 
 
 if __name__ == "__main__":
