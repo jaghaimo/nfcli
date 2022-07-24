a77cbad4243b46689e08b7e9bd052bb5c0522a55
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-07-24 09:21:57 +0100

    Remove unused

--- nfcli/extractor.py
+++ nfcli/extractor.py
@@ -2,7 +2,6 @@ import json
 import logging
 from typing import Dict
 
-from nfcli import load_path
 from nfcli.model import Fleet, Ship
 
 
@@ -31,42 +30,3 @@ def extract_slots(output: str, fleet: Fleet):
         add_ship(info, ship)
     with open(output, "w") as file:
         json.dump(info, file, indent=4)
-
-
-def get_sockets(ship_data: Dict) -> Dict:
-    sockets = {}
-    for key, value in ship_data.items():
-        sockets[key] = "x".join([str(x) for x in value.get("size")])
-    return sockets
-
-
-def convert_nfm_data():
-    """This is a one off to convert Nebulous Fleet Manager android app data."""
-    convert_ships()
-    convert_components()
-
-
-def convert_ships():
-    stock_ships = load_path("data/stock_ships_old.json")
-    ships = json.loads(stock_ships)
-    new_data = {}
-    for ship_name, ship_data in ships.items():
-        data = {
-            "name": ship_name,
-            "mounts": get_sockets(ship_data.get("mountkeys")),
-            "compartments": get_sockets(ship_data.get("compartmentkeys")),
-            "modules": get_sockets(ship_data.get("modulekeys")),
-        }
-        new_data[ship_name] = data
-    with open("data/stock_ships.json", "w") as file:
-        json.dump(new_data, file, indent=4)
-
-
-def convert_components():
-    stock_components = load_path("data/stock_components_old.json")
-    components = json.loads(stock_components)
-    new_data = {}
-    for component_name, component_data in components.items():
-        new_data[component_name] = component_data.get("category")
-    with open("data/stock_components.json", "w") as file:
-        json.dump(new_data, file, indent=4)
