7f3e6d95131b7b633f5037a42daedc8e44aecd9f
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-07-25 14:39:20 +0100

    Cleanup stock names

--- nfcli/extractor.py
+++ nfcli/extractor.py
@@ -33,13 +33,11 @@ def get_ship(entry: object, all_entries: Dict[str, object]) -> Dict:
     name = entry._className
     hull = entry._hullClassification
     ship = {"name": name, "hull": hull, "mounts": {}, "compartments": {}, "modules": {}}
-
     socket_root = str(entry._socketRoot["fileID"])
     socket_node = all_entries[socket_root]
     for child in socket_node.m_Children:
         socket = get_socket(str(child["fileID"]), all_entries)
         add_socket(ship, socket)
-
     return ship
 
 
@@ -48,13 +46,11 @@ def extract_ship_data(input: str):
     all_entries = {}
     for entry in doc.entries:
         all_entries[str(entry.anchor)] = entry
-
     entries = doc.filter(
         class_names=("MonoBehaviour",), attributes=("_className", "_hullClassification", "_socketRoot")
     )
     if len(entries) != 1:
         raise RuntimeError("Could not find a starting MonoBehaviour object")
-
     return get_ship(entries[0], all_entries)
 
 def extract_from_prefabs():
