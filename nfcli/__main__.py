5a530c91287e1c7ffe7db48be68379b9576a1127
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-07-25 07:44:26 +0100

    Naive implementation

--- nfcli/__main__.py
+++ nfcli/__main__.py
@@ -6,8 +6,7 @@ from nfcli import init_logger, load_path
 from nfcli.extractor import extract_slots
 from nfcli.parser import parse_any, parse_mods
 from nfcli.printer import printer_factory
-from nfcli.steam import cache_workshop_files, get_workshop_files
-from nfcli.wiki import update_wiki
+from nfcli.steam import get_workshop_files
 from nfcli.writer import determine_output_png
 
 DESC = """Command line interface for converting Nebulous: Fleet Command fleet and ship files to images."""
@@ -21,8 +20,6 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument("-p", "--print", action="store_true", help="print output to console")
     parser.add_argument("-s", "--style", type=str, default="auto", help="printer style: auto (default), column, stack")
     parser.add_argument("-w", "--write", action="store_true", help="write output to a file")
-    parser.add_argument("--update-wiki", action="store_true", help="update wiki database")
-    parser.add_argument("--update-workshop", action="store_true", help="update Steam Workshop cache")
     parser.add_argument("--workshop", type=int, help="download and parse Steam Workshop fleet")
     return parser
 
@@ -38,10 +35,8 @@ def parse_args() -> Dict:
 def main() -> int:
     args = parse_args()
     entity = None
-    if args.update_wiki:
-        update_wiki()
-    if args.update_workshop:
-        cache_workshop_files()
+    if args.extract:
+        extract_slots(args.input, args.extract)
     if args.workshop:
         input_files = get_workshop_files(args.workshop)
         args.input = input_files[0] if input_files else None
@@ -49,8 +44,6 @@ def main() -> int:
         xml_data = load_path(args.input)
         entity = parse_any(args.input, xml_data)
     if entity:
-        if args.extract:
-            extract_slots(args.extract, entity)
         if args.print:
             printer = printer_factory(args.style, entity)
             entity.print(printer)
--- nfcli/extractor.py
+++ nfcli/extractor.py
@@ -1,32 +1,55 @@
 import json
 import logging
+import os
+from glob import glob
+from posixpath import basename
 from typing import Dict
 
+from ruamel.yaml import YAML
+
+from nfcli import load_path
 from nfcli.model import Fleet, Ship
+from nfcli.parser import parse_fleet
 
 
-def add_ship(info: Dict, ship: Ship):
+def add_ship(info: Dict, ship: Ship, socket_data: Dict):
     ship_info = {"name": ship.name}
     ship_info["mounts"] = {}
     ship_info["compartments"] = {}
     ship_info["modules"] = {}
     for key, socket in ship.sockets.items():
+        socket_size = socket_data.get(socket.key) if socket.key in socket_data else "?x?x?"
         if socket.name == "CR10 Antenna":
-            ship_info["mounts"][socket.key] = "?x?x?"
+            ship_info["mounts"][socket.key] = socket_size
         elif socket.name == "Auxiliary Steering":
-            ship_info["compartments"][socket.key] = "?x?x?"
+            ship_info["compartments"][socket.key] = socket_size
         elif socket.name == "Supplementary Radio Amplifiers":
-            ship_info["modules"][socket.key] = "?x?x?"
+            ship_info["modules"][socket.key] = socket_size
         else:
             logging.warn(f"Unrecognized socket {key} which contains {socket.name}")
     info[ship._hull] = ship_info
 
 
-def extract_slots(output: str, fleet: Fleet):
+def get_socket_data(input: str) -> Dict:
+    path = os.path.join("prefab", basename(input)[:-6], "*.json")
+    socket_data = {}
+    for filename in glob(path):
+        raw_data = load_path(filename)
+        hull_data = json.loads(raw_data)
+        key = hull_data["_key"]
+        value = hull_data["_size"]
+        socket_data[key] = "x".join([str(x) for x in value.values()])
+    return socket_data
+
+
+def extract_slots(input: str, output: str):
+    xml_data = load_path(input)
+    fleet = parse_fleet(xml_data)
     if not isinstance(fleet, Fleet):
         raise ValueError("Not a Fleet type")
     info = {}
+    socket_data = get_socket_data(input)
     for ship in fleet.ships:
-        add_ship(info, ship)
+        add_ship(info, ship, socket_data)
     with open(output, "w") as file:
         json.dump(info, file, indent=4)
