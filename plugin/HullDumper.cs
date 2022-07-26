379f018bb7e2adb6cbbe8f993c0d6d9e0eb5d571
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-07-26 16:33:00 +0100

    Add game plugin for extracting data

---
+++ plugin/HullDumper.cs
@@ -0,0 +1,92 @@
+﻿using Bundles;
+using Modding;
+using Ships;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HullDumper
+{
+    public class HullDumper : IModEntryPoint
+    {
+        public void PostLoad()
+        {
+            IReadOnlyCollection<Hull> allHulls = BundleManager.Instance.AllHulls;
+            foreach (Hull hull in allHulls)
+            {
+                ulong sourceModId = hull.SourceModId ?? 0;
+                string fileName = $"{sourceModId}_{hull.ClassName.ToLower()}";
+                HullSpec hullSpec = GetHull(hull);
+                System.IO.File.WriteAllText($"{ModDatabase.LocalModDirectory}{fileName}.json", hullSpec.ToString());
+            }
+        }
+        public void PreLoad()
+        {
+        }
+
+        private HullSpec GetHull(Hull hull)
+        {
+            HullSpec hullSpec = new HullSpec(hull.FullClassification);
+            foreach (HullSocket socket in hull.GetAllSockets())
+            {
+                SocketSpec socketSpec = new SocketSpec { Key = socket.Key, Size = socket.Size };
+                hullSpec.Add(socket.Type, socketSpec);
+            }
+            Console.WriteLine(hullSpec);
+            return hullSpec;
+        }
+    }
+    public class HullSpec
+    {
+        public HullSpec(string name)
+        {
+            Name = name;
+            Surfaces = new List<SocketSpec>();
+            Compartments = new List<SocketSpec>();
+            Modules = new List<SocketSpec>();
+        }
+        public string Name { get; set; }
+        public List<SocketSpec> Surfaces { get; set; }
+        public List<SocketSpec> Compartments { get; set; }
+        public List<SocketSpec> Modules { get; set; }
+        public void Add(HullSocketType type, SocketSpec socket)
+        {
+            switch (type)
+            {
+                case HullSocketType.Surface:
+                    Surfaces.Add(socket);
+                    break;
+                case HullSocketType.Compartment:
+                    Compartments.Add(socket);
+                    break;
+                case HullSocketType.Module:
+                    Modules.Add(socket);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private string SocketsToString(List<SocketSpec> sockets)
+        {
+            return "    " + string.Join(",\n    ", sockets);
+        }
+        public override string ToString()
+        {
+            return "{\n" +
+                $"  \"name\": \"{Name}\",\n" +
+                "  \"mounts\": {\n" + SocketsToString(Surfaces) + "\n  },\n" +
+                "  \"compartments\": {\n" + SocketsToString(Compartments) + "\n  },\n" +
+                "  \"modules\": {\n" + SocketsToString(Modules) + "\n  }\n" +
+                "}\n";
+        }
+    }
+    public class SocketSpec
+    {
+        public string Key { get; set; }
+        public Vector3Int Size { get; set; }
+        public override string ToString()
+        {
+            return $"\"{Key}\": \"{Size.x}x{Size.y}x{Size.z}\"";
+        }
+    }
+}
