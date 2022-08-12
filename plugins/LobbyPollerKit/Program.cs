325d1728e493fd4b3219013c684c7a93770bf444
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-08-12 19:44:51 +0100

    Add basic project

---
+++ plugins/LobbyPollerKit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Steamworks;
+
+SteamClient.Init(877570);
+if (SteamClient.IsValid)
+{
+    Console.WriteLine("Hello, World! Steam connected.");
+}
+else
+{
+    Console.WriteLine("Not Hello, World! Steam failed.");
+}
